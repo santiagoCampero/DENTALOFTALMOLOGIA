2215f610ca2558a1dcd9ef624bad2d90c3ed9ef0
santiago.campero <santiago.campero@mediaccess.com.mx>
2017-11-22 16:55:10 -0600

    Se crea proyecto Dental-Oftalmologia (DENOF) para trabajo colaboratitvo

---
+++ DENOF/DENOF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DENOF.Startup))]
+namespace DENOF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
